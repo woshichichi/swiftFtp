643d8a6d07baaab2f4ffbf812399c6c193a9049b
woshichichi <88015309+woshichichi@users.noreply.github.com>
2025-07-31 20:08:48 +0800

    23

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "SwiftFTPClient",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v13),
+        .iOS(.v15),
         .tvOS(.v13),
         .watchOS(.v6)
     ],
